6e3f5ff7f1fb6df1c53138410c9866aac07ce911
Manomay Gadiyar <manomay.gadiyar@gmail.com>
2026-03-31 17:45:26 +0530

    Updated app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Name: Manomay Gadiyar | AppID: 2400579 | Hobbies: Gaming"
+    return "Name: Manomay Gadiyar | AppID: 2400579 | Hobbies: Coding"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
